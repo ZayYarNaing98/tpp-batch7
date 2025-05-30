61dfbeb57d009eb146bf8b0056916e0740cd4883
ZayYarNaing <zayyarnaing@aceplussolutions.com>
2025-05-30 15:53:13 +0630

    Add Boostrap UI in index category

--- routes/web.php
+++ routes/web.php
@@ -46,7 +46,6 @@ Route::prefix('/dashboard')->group(function(){
 // });
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('category.index');
-// Route::get('/categories/{id}', [CategoryController::class, 'show'])->name('categories.show');
 
 Route::get('/categories/{id}/edit', [CategoryController::class, 'edit'])->name('categories.edit');
 Route::post('/categories/{id}/update', [CategoryController::class, 'update'])->name('categories.update');
