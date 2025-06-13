b156d3f66416809045035ff8391f61b5f579337f
ZayYarNaing <zayarnaing.pp@gmail.com>
2025-06-13 16:08:55 +0630

    Add Login and Register

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::post('/auth/register', [AuthController::class, 'register']);
